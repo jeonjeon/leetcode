a96b6d6ecec6b83b7ff973bbde9e4312f9e06dd8
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-05-27 09:52:47 +0900

    Time: 335 ms (100.00%), Space: 150.2 MB (100.00%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int specialArray(List<int> nums) {
+    final candi = List.filled(1001, 0);
+    nums.forEach((n) => candi[n]++);
+    for (int i = 999; i >= 0; i--){
+      candi[i] += candi[i + 1];
+    }
+    for (int i = nums.length; i > 0; i--){
+      if (candi[i] == i) return i;
+    }
+    return -1;
+  }
+}
