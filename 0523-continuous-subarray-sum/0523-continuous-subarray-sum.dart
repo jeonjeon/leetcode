7f90e4f38e8dda6130f814ce7a3d05d0831e6117
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-06-08 15:35:22 +0900

    Time: 429 ms (100.00%), Space: 204.4 MB (100.00%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.dart
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.dart
@@ -1,16 +1,15 @@
 class Solution {
   bool checkSubarraySum(List<int> nums, int k) {
     final ps = <int>{};
-    int sum = nums.first;
-    int prev = sum % k;
+    int sum = nums.first % k;
+    int prev = sum;
     for (int i = 1; i < nums.length; i++){
-      sum += nums[i];
-      final mod = sum % k;
-      if (mod == 0 || ps.contains(mod)){
+      sum = (sum + nums[i]) % k;
+      if (sum == 0 || ps.contains(sum)){
         return true;
       }
       ps.add(prev);
-      prev = mod;
+      prev = sum;
     }
     return false;
   }
