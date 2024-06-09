3ee3ab15f1c35c88ffc93eb5f6dabba81d1a9df4
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-06-09 16:27:13 +0900

    Time: 394 ms (100.00%), Space: 155.4 MB (100.00%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.dart
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.dart
@@ -1,19 +1,30 @@
 import 'dart:math';
 class Solution {
   int minimumDeleteSum(String s1, String s2) {
-    final arr1 = s1.split('').map((a)=> a.codeUnitAt(0)).toList();
-    final arr2 = s2.split('').map((a)=> a.codeUnitAt(0)).toList();
-    final every = arr1.reduce((a,b) => a + b) + arr2.reduce((a, b) => a + b);
-    final dp = List.generate(s1.length + 1, (_) => List.filled(s2.length + 1, every));
-    for (int i = 1; i < dp.length; i++){
-      for (int j = 1; j < dp[0].length; j++){
-        if (arr1[i - 1] == arr2[j - 1]){
-          dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] - arr1[i - 1] - arr2[j - 1]);
-          continue;
+    final n1 = s1.length + 1;
+    final n2 = s2.length + 1;
+    final dp = List.generate(n1, (_) => List.filled(n2, 0));
+    for (int i = 1; i < n1; i++){
+      dp[i][0] = dp[i - 1][0] + s1.codeUnitAt(i - 1);
+    }
+    for (int i = 1; i < n2; i++){
+      dp[0][i] = dp[0][i - 1] + s2.codeUnitAt(i - 1);
+    }
+    for(int i = 1; i < n1; i++){
+      for (int j = 1; j < n2; j++){
+        if (s1[i - 1] == s2[j - 1]){
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = min(dp[i - 1][j] + s1.codeUnitAt(i - 1), dp[i][j - 1] + s2.codeUnitAt(j - 1));
         }
-        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]);
       }
     }
     return dp.last.last;
   }
 }
+//[
+//  [0, 101, 198, 314],
+// [115, 202, 295, 430],
+// [216, 115, 392, 546],
+// [313, 216, 115, 662]
+//]
