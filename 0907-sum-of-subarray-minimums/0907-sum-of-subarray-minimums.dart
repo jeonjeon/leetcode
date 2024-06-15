c1fda6782c18f7302d09cbf582d6d0884d462dce
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-06-15 12:21:22 +0900

    Time: 364 ms (100%), Space: 160.5 MB (100%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.dart
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.dart
@@ -1,31 +1,20 @@
 class Solution {
     static const MOD = 1000000007;
   int sumSubarrayMins(List<int> arr) {
-//       앞에게 작을때는 left++
-//       앞에게 더 클때는 res += left
-      final lessIdx = List<List<int>>.generate(arr.length, (_) => [-1,-1]);
-      final preStack = <int>[];
-      final subStack = <int>[];
-      int res = 0;
-      for (int i = 0; i < arr.length; i++){
-          final reverseIdx = arr.length - i - 1;
-//           앞쪽
-          while (preStack.isNotEmpty && arr[preStack.last] >= arr[i]){
-              preStack.removeLast();
-          }
-//           뒤쪽
-          while (subStack.isNotEmpty && arr[subStack.last] > arr[reverseIdx]){
-              subStack.removeLast();
-          }
-          lessIdx[i][0] = preStack.isEmpty ? -1 : preStack.last;
-          lessIdx[reverseIdx][1] = subStack.isEmpty ? arr.length : subStack.last;
-          preStack.add(i);
-          subStack.add(reverseIdx);
+    int res = 0;
+    int total = 0;
+    final stack = <(int,int)>[];
+    for (final a in arr){
+      int curCnt = 1;
+      while (stack.isNotEmpty && stack.last.$1 > a){
+        final (val, cnt) = stack.removeLast();
+        total -= val * cnt;
+        curCnt += cnt;
       }
-      for (int i = 0; i < lessIdx.length; i++){  
-          res += ((i - lessIdx[i][0]) * (lessIdx[i][1] - i) * arr[i]) % MOD;
-          res = res % MOD;
-      }
-      return res;
+      total += a * curCnt;
+      res += total;
+      stack.add((a, curCnt));
+    }
+    return res % MOD;
   }
 }
