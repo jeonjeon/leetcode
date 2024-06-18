91eae5aff42292eae1b8a7e216d578e2f6ea52d0
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-06-18 10:27:22 +0900

    Time: 502 ms (100%), Space: 160.2 MB (100%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.dart
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.dart
@@ -3,23 +3,14 @@ import 'dart:math';
 class Solution {
   int maxProfitAssignment(List<int> diff, List<int> profit, List<int> worker) {
     int res = 0;
-    final work= SplayTreeMap<int, int>((a, b) => b.compareTo(a));
     final job = SplayTreeMap<int, int>((a, b) => b.compareTo(a));
     for (int i = 0; i < diff.length; i++){
       job[profit[i]] = min((job[profit[i]] ?? 100001), diff[i]);
     }
-    for (int i = 0; i < worker.length; i++){
-      work[worker[i]] = (work[worker[i]] ?? 0) + 1;
-    }
+    worker.sort((a,b) => b.compareTo(a));
     //print('job: $job');
-    //print('work: $work');
-    while (work.isNotEmpty){
-      final abil = work.firstKey()!;
-      final cnt = work[abil]!;
-      work[abil] = cnt - 1;
-      if (work[abil] == 0){
-        work.remove(abil);
-      } 
+    //print('worker: $worker');
+    for (final abil in worker){
       while (job.isNotEmpty){
         final key = job.firstKey()!;
         final j = job[key]!;
