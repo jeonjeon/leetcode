44652222552c90c3a5718825bfe1f306fd095557
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-07-18 13:57:15 +0900

    Time: 357 ms (100%), Space: 151.6 MB (100%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.dart
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.dart
@@ -7,64 +7,40 @@
  *   TreeNode([this.val = 0, this.left, this.right]);
  * }
  */
-import 'dart:collection';
 class Solution {
-  int countPairs(TreeNode? root, int dist) {
-    final edges = getEdges(root!);
-    if (edges.length == 1) return 0;
-    return goodPairs(edges, dist);
-  }
-  int goodPairs(Map<int, List<int>> edges, int dist){
-    int res = 0;
-    final leaves = edges.entries
-                        .where((e) => e.value.length == 1)
-                        .map((e) => e.key)
-                        .toSet();
-    leaves.remove(0);
-    var q = Queue<(int, int)>.from(leaves.map((e) => (e, e)));
-    int cnt = 0;
-    while (q.isNotEmpty && cnt < dist){
-      final temp = Queue<(int, int)>();
-      while (q.isNotEmpty){
-        final (cur, prev) = q.removeFirst();
-        final next = edges[cur]!;
-        for (final n in next){
-          if (n == prev) continue;
-          if (leaves.contains(n)) {
-            res++;
-            continue;
-          }
-          temp.add((n, cur));
+  int res = 0;
+
+  List<int> dfs(TreeNode? node, int distance) {
+    List<int> map = List<int>.filled(11, 0); // Initialize list with 11 elements filled with 0s
+
+    if (node == null) return map;
+
+    if (node.left == null && node.right == null) {
+      map[1] = 1;
+      return map;
+    }
+
+    List<int> left = dfs(node!.left, distance); // Use node! to assert non-null
+    List<int> right = dfs(node.right, distance); // node.right is already non-null
+
+    for (int i = 1; i <= distance; i++) {
+      for (int j = 1; j <= distance; j++) {
+        if (i + j <= distance) {
+          res += left[i] * right[j];
         }
       }
-      cnt++;
-      q = temp;
     }
-    return res ~/ 2;
+
+    for (int i = 2; i < 11; i++) {
+      map[i] += left[i - 1] + right[i - 1];
+    }
+
+    return map;
   }
-  Map<int, List<int>> getEdges(TreeNode root){
-    int cnt = 0;
-    root.val = cnt;
-    final edges = <int, List<int>>{};
-    final q = Queue<TreeNode>()..add(root!);
-    while (q.isNotEmpty){
-      final cur = q.removeFirst();
-      edges[cur.val] ??= [];
-      if (cur.left != null){
-        cnt++;
-        cur.left!.val = cnt;
-        edges[cur.val]!.add(cnt);
-        edges[cnt] = [cur.val];
-        q.add(cur.left!);
-      }
-      if (cur.right != null){
-        cnt++;
-        cur.right!.val = cnt;
-        edges[cur.val]!.add(cnt);
-        edges[cnt] = [cur.val];
-        q.add(cur.right!);
-      }
-    }
-    return edges;
+
+  int countPairs(TreeNode? root, int distance) {
+    if (root == null) return 0;
+    dfs(root, distance);
+    return res;
   }
 }
