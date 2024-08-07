0909e6b517f728bef9dd3dea1851e37235051236
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-08-07 14:38:09 +0900

    Time: 328 ms (100%), Space: 151.7 MB (100%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.dart
+++ 0273-integer-to-english-words/0273-integer-to-english-words.dart
@@ -1,7 +1,8 @@
 class Solution {
-    final under20 = { 0: "", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen"}; 
-    final over20 = {20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety"};
-    final over100 = [(1000000000, "Billion"), (1000000, "Million"), (1000, "Thousand"), (100, "Hundred")];
+  final under20 = { 0: "", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen"}; 
+  final over20 = {20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety"};
+  final over100 = [(1000000000, "Billion"), (1000000, "Million"), (1000, "Thousand"), (100, "Hundred")];
+
   String numberToWords(int n) {
     if (n == 0) return 'Zero';
     var res = '';
@@ -14,9 +15,9 @@ class Solution {
       } 
       if (n >= 20){
         res += ' ${over20[(n ~/ 10) * 10]} ${under20[n % 10]}';
-      } else {
-        res += ' ${under20[n]}';
-      }
+        break;
+      } 
+      res += ' ${under20[n]}';
       break;
     }
     return res.trim();
