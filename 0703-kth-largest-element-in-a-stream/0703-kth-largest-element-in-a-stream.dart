24842be4e6b7bfe4babb84f56413b1657a17c6dd
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-08-12 13:08:45 +0900

    Time: 421 ms (100%), Space: 171 MB (80%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.dart
@@ -0,0 +1,85 @@
+class KthLargest {
+  final int k;
+  final pq = PriorityQueue<int>((a, b) => a.compareTo(b));
+  KthLargest(this.k, List<int> nums) {
+    nums.forEach(pq.add);
+  }
+  
+  int add(int val) {
+    pq.add(val);
+    while(pq.length > k){
+      pq.remove();
+    }
+    return pq.first;
+  }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = KthLargest(k, nums);
+ * int param1 = obj.add(val);
+ */
+
+ class PriorityQueue<T> { 
+	List<T> _elements = []; 
+	Comparator<T> _comparator; 
+	PriorityQueue(this._comparator); 
+	
+	void add(T element) { 
+		_elements.add(element); 
+		_bubbleUp(_elements.length - 1); 
+	} 
+	
+	T remove() { 
+		if (isEmpty) throw StateError("Cannot remove from empty priority queue."); 
+		T result = _elements.first; 
+		if (_elements.length == 1) { 
+			_elements.removeLast(); 
+		} else { 
+			_elements[0] = _elements.removeLast(); 
+			_bubbleDown(0); 
+		} return result; 
+	} 
+		
+	T get first { 
+		if (isEmpty) throw StateError('empty'); 
+		return _elements.first; 
+	} 
+	
+	bool get isEmpty => _elements.isEmpty; 
+	
+	int get length => _elements.length; 
+	
+	void _bubbleUp(int index) { 
+		while (index > 0) { 
+			int parent = (index - 1) >> 1; 
+			if (_comparator(_elements[index], _elements[parent]) >= 0) break; 
+			_swap(index, parent); index = parent; 
+		} 
+	} 
+	
+	void _bubbleDown(int index) { 
+		int leftChild; 
+		int rightChild; 
+		int minChild; 
+		while (true) { 
+			leftChild = (index << 1) + 1; 
+			rightChild = leftChild + 1; 
+			minChild = index; 
+			if (leftChild < _elements.length && _comparator(_elements[leftChild], _elements[minChild]) < 0) { 
+				minChild = leftChild; 
+			} 
+			if (rightChild < _elements.length && _comparator(_elements[rightChild], _elements[minChild]) < 0) { 
+				minChild = rightChild; 
+			} 
+			if (minChild == index) break; 
+			_swap(index, minChild); index = minChild; 
+		}
+	} 
+	
+	void _swap(int i, int j) { 
+		T temp = _elements[i]; 
+		_elements[i] = _elements[j]; 
+		_elements[j] = temp; 
+	} 
+}
