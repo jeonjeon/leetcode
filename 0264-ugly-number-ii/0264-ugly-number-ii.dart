2292748a260b508895120c3571f43a054db22668
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-08-18 23:35:24 +0900

    Time: 342 ms (100%), Space: 149.5 MB (100%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.dart
+++ 0264-ugly-number-ii/0264-ugly-number-ii.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 class Solution {
   int nthUglyNumber(int n) {
     int i2 = 0, i3 = 0, i5 = 0;
-    final arr = <int>[1];
+    final arr = [1];
     while (arr.length < n){
       final m = arr.add(min(arr[i2] * 2, min(arr[i3] * 3, arr[i5] * 5)));
       if (arr[i2] * 2 == arr.last) i2++;
