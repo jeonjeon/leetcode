2d22dc8fcc10175b8a4b867f5398c6c1443013cf
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-08-18 23:53:18 +0900

    Time: 443 ms (100%), Space: 149.1 MB (100%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.dart
+++ 0264-ugly-number-ii/0264-ugly-number-ii.dart
@@ -1,14 +1,16 @@
-import 'dart:math';
+import 'dart:collection';
 class Solution {
   int nthUglyNumber(int n) {
-    int i2 = 0, i3 = 0, i5 = 0;
-    final arr = [1];
-    while (arr.length < n){
-      final m = arr.add(min(arr[i2] * 2, min(arr[i3] * 3, arr[i5] * 5)));
-      if (arr[i2] * 2 == arr.last) i2++;
-      if (arr[i3] * 3 == arr.last) i3++;
-      if (arr[i5] * 5 == arr.last) i5++;
+    final sts = SplayTreeSet<int>((a, b) => a.compareTo(b));
+    int cur = 1;
+    sts.add(1);
+    for (int i = 0; i < n; i++){
+      cur = sts.first;
+      sts.remove(cur);
+      sts.add(cur * 2);
+      sts.add(cur * 3);
+      sts.add(cur * 5);
     }
-    return arr.last;
+    return cur;
   }
 }
