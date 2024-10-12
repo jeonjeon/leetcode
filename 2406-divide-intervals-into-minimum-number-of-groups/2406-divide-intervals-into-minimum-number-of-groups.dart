c6d5e9efecde4db79cf4c0fcd9531eba2879daf1
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-10-12 16:54:07 +0900

    Time: 608 ms (100%), Space: 231.9 MB (100%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.dart
@@ -0,0 +1,82 @@
+import 'dart:math';
+class Solution {
+  int minGroups(List<List<int>> intv) {
+    intv.sort((a, b) {
+      if (a[0] == b[0]){
+        return a[1].compareTo(b[1]);
+      }
+      return a[0].compareTo(b[0]);
+    });
+    final pq = PriorityQueue<int>((a, b) => a.compareTo(b));
+    for (final i in intv){
+      if (!pq.isEmpty && pq.first < i[0]){
+        pq.remove();
+      }
+      pq.add(i[1]);
+    }
+    return pq.length;
+  }
+}
+class PriorityQueue<T> { 
+	List<T> _elements = []; 
+	Comparator<T> _comparator; 
+	PriorityQueue(this._comparator); 
+	
+	void add(T element) { 
+		_elements.add(element); 
+		_bubbleUp(_elements.length - 1); 
+	} 
+	
+	T remove() { 
+		if (isEmpty) throw StateError("Cannot remove from empty priority queue."); 
+		T result = _elements.first; 
+		if (_elements.length == 1) { 
+			_elements.removeLast(); 
+		} else { 
+			_elements[0] = _elements.removeLast(); 
+			_bubbleDown(0); 
+		} return result; 
+	} 
+		
+	T get first { 
+		if (isEmpty) throw StateError('empty'); 
+		return _elements.first; 
+	} 
+	bool get isNotEmpty => !_elements.isEmpty; 
+	bool get isEmpty => _elements.isEmpty; 
+	
+	int get length => _elements.length; 
+	
+	void _bubbleUp(int index) { 
+		while (index > 0) { 
+			int parent = (index - 1) >> 1; 
+			if (_comparator(_elements[index], _elements[parent]) >= 0) break; 
+			_swap(index, parent); index = parent; 
+		} 
+	} 
+	
+	void _bubbleDown(int index) { 
+		int leftChild; 
+		int rightChild; 
+		int minChild; 
+		while (true) { 
+			leftChild = (index << 1) + 1; 
+			rightChild = leftChild + 1; 
+			minChild = index; 
+			if (leftChild < _elements.length && _comparator(_elements[leftChild], _elements[minChild]) < 0) { 
+				minChild = leftChild; 
+			} 
+			if (rightChild < _elements.length && _comparator(_elements[rightChild], _elements[minChild]) < 0) { 
+				minChild = rightChild; 
+			} 
+			if (minChild == index) break; 
+			_swap(index, minChild); index = minChild; 
+		}
+	} 
+	
+	void _swap(int i, int j) { 
+		T temp = _elements[i]; 
+		_elements[i] = _elements[j]; 
+		_elements[j] = temp; 
+	} 
+}
