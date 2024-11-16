e27afa356a451b9757ceae8a0650b2a3cebd85f3
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2024-11-16 10:42:22 +0900

    Time: 3 ms (100%), Space: 151.2 MB (100%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.dart
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.dart
@@ -1,34 +1,24 @@
-import 'dart:collection';
 class Solution {
   List<int> resultsArray(List<int> nums, int k) {
-    final res = <int>[];
     if (k == 1) return nums;
+
+    final res = <int>[];
     final n = nums.length;
     final prefix = List.filled(n, 1);
-    final stm = SplayTreeMap<int, int>((a, b) => b.compareTo(a));
-    int l = 0;
-    for (int i = 0; i < n; i++){
-         stm[nums[i]] = (stm[nums[i]] ?? 0) + 1;
-         if (i == 0) continue;
+    
+    for (int i = 1; i < n; i++){
          if (nums[i] - nums[i - 1] == 1){
             prefix[i] += prefix[i - 1];
          }
+
          if (i < k - 1) continue;
+
          if (prefix[i] >= k){
-            res.add(stm.firstKey()!);
+            res.add(nums[i]);
          } else {
             res.add(-1);
          }
-        //  print('stm: $stm, l: $l');
-         if (stm[nums[l]]! == 1){
-            stm.remove(nums[l]);
-         } else {
-            stm[nums[l]] = stm[nums[l]]! - 1;
-         }
-         l++;
     }
-    // print('stm: $stm');
-    // print('prefix: $prefix');
     return res;
   }
 }
