3d9699b9f3b7b398d87d659b50d3d273ee46acc9
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-03-01 11:02:14 +0900

    Time: 1 ms (71.43%), Space: 148.6 MB (57.14%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.dart
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.dart
@@ -1,17 +1,20 @@
 class Solution {
   List<int> applyOperations(List<int> nums) {
-    final n = nums.length;
-    int ri = 0;
-    final res = List.filled(n, 0);
-    for (int i = 0; i < nums.length; i++){
-      if (i < n - 1 && nums[i] == nums[i + 1]){
-        res[ri] = nums[i] * 2;
-        nums[i + 1] = 0;
-      } else {
-        res[ri] = nums[i];
+    int l = 0, r = 0, n = nums.length;
+    while (l < n){
+      if (r == n) {
+        nums[l] = 0;
+        l++;
+        continue;
       }
-      if (res[ri] > 0) ri++;
+      nums[l] = nums[r];
+      if (r < n - 1 && nums[r] == nums[r + 1]){
+        nums[l] *= 2;
+        nums[r + 1] = 0;
+      }
+      if (nums[l] > 0) l++;
+      r++;
     }
-    return res;
+    return nums;
   }
 }
