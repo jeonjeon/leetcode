a49b4bfae1ace7554fb159255e4618c8b562f8c3
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-03-12 17:17:08 +0900

    Time: 0 ms (100%), Space: 152.8 MB (12.5%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.dart
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.dart
@@ -1,24 +1,22 @@
 import 'dart:math';
 class Solution {
   int maximumCount(List<int> nums) {
-    final len = nums.length;
-    if (nums.first > 0 || nums.last < 0) return len;
-    int l = 0, r = len - 1;
+    if (nums.first > 0 || nums.last < 0) return nums.length;
+    final l = bs(nums, 0);
+    final r = bs(nums, 1);
+    // print('l: $l, r: $r, len: ${nums.length}');
+    return max(l, nums.length - r);
+  }
+  int bs(List<int> nums, int target){
+    int l = 0, r = nums.length;
     while (l < r){
         final m = (l + r) ~/ 2;
-        if (nums[m] >= 0){
+        if (nums[m] >= target){
             r = m;
         } else {
             l =  m + 1;
         }
     }
-    while (l >= 0 && nums[l] > -1){
-        l--;
-    }
-    while (r < len && nums[r] < 1){
-        r++;
-    }
-    // print('l: $l, r: $r');
-    return max(l + 1, len - r);
+    return l;    
   }
 }
