b31a9f6d7b2b45c19aef29a1144b01b465487fcf
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-04-06 11:29:52 +0900

    Time: 58 ms (100%), Space: 156.2 MB (100%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.dart
@@ -0,0 +1,19 @@
+class Solution {
+  List<int> largestDivisibleSubset(List<int> nums) {
+    int idx = 0;
+    final nms = nums.map((n) => (idx++, n)).toList()..sort((a, b) => a.$2.compareTo(b.$2));
+    final idxs = <List<int>>[];
+    for (int i = 0; i < nms.length; i++){
+      final (id, n) = nms[i];
+      idxs.add([id]);
+      for (int j = i - 1; j >= 0; j--){
+        final (pId, pN) = nms[j];
+        if (n % pN == 0 && idxs[i].length <= idxs[j].length) {
+          idxs[i] = [...idxs[j], id];
+        }
+      }
+    }
+    final largest = idxs.reduce((a, b) => a.length > b.length ? a : b);
+    return largest.map((l) => nums[l]).toList();
+  }
+}
