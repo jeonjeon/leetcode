3ce6be010d79acd257839502fd32adec3e62aceb
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-04-16 17:35:42 +0900

    Time: 53 ms (100%), Space: 186.6 MB (100%) - LeetHub

---
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.dart
@@ -0,0 +1,18 @@
+class Solution {
+  int countGood(List<int> nums, int k) {
+    int l = 0, pairs = 0, res = 0;
+    final hm = <int, int>{};
+    for (int r = 0; r < nums.length; r++){
+      hm[nums[r]] = (hm[nums[r]] ?? 0) + 1;
+      pairs += hm[nums[r]]! - 1;
+      while (l < nums.length && pairs >= k && pairs - hm[nums[l]]! + 1 >= k){
+        pairs -= hm[nums[l]]! - 1;
+        hm[nums[l]] = hm[nums[l]]! - 1;
+        if (hm[nums[l]] == 0) hm.remove(nums[l]);
+        l++;
+      }
+      if (pairs >= k) res += l + 1;
+    }
+    return res;
+  }
+}
