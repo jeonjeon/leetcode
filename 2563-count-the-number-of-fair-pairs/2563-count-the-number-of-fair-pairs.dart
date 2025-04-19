c8b09c5c3489d8c9d202dbc6060e4cb6fd9c695d
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-04-19 11:54:59 +0900

    Time: 160 ms (0%), Space: 181.2 MB (0%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.dart
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.dart
@@ -3,14 +3,13 @@ class Solution {
   int countFairPairs(List<int> nums, int lower, int upper) {
     nums.sort();
     print(nums);
-    int l = nums.length - 1, r = nums.length - 1;
-    int res = 0;
+    int res = 0, l = nums.length - 1, r = nums.length - 1;
     for (int i = 0; i < nums.length; i++){
-        while (i < l && nums[i] >= lower - nums[l]) l--;
-        while (i < r && nums[i] > upper - nums[r]) r--;
+        while (i < l && nums[i] + nums[l] >= lower) l--;
+        while (l < r && nums[i] + nums[r] > upper) r--;
         if (r <= i) break;
-        // print('i: $i, l: $l, r: $r');
         res += r - max(i, l);
+        // print('nums[i]: ${nums[i]}, nums[l]: ${nums[l]}, nums[r]: ${nums[r]}, res: $res');
     }
     return res;
   }
