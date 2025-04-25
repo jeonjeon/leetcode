6ca276ba4d2c9fac5021ddfc1e7d8bb8439f1e48
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-04-25 18:07:31 +0900

    Time: 44 ms (100%), Space: 191.5 MB (100%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.dart
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.dart
@@ -1,22 +1,16 @@
 class Solution {
   int countInterestingSubarrays(List<int> nums, int mod, int k) {
-    int res = 0;
-    int cnt = 0;
     final hm = <int, int>{};
-
-    for (int i = 0; i < nums.length; i++){
-      // count n mod == k
-      if (nums[i] % mod == k){
-        cnt = (cnt + 1) % mod;
-      }
-      int pc = cnt - k;
-      if (pc < 0) pc += mod;
-      res += hm[pc] ?? 0;
-      if (cnt == k) res++;
-      //print('cnt: $cnt, pc: $pc, hm: $hm');
-      hm[cnt] = (hm[cnt] ?? 0) + 1;
+    int res = 0, cnt = 0;
+    for (final n in nums){
+        if (n % mod == k) cnt++;
+        cnt %= mod;
+        if (cnt == k) res++;
+        int cur = cnt - k;
+        if (cur < 0) cur += mod;
+        res += hm[cur] ?? 0;
+        hm[cnt % mod] = (hm[cnt % mod] ?? 0) + 1;
     }
-    //print(hm);
     return res;
   }
 }
