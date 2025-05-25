dcddac76f4138a5bf65a7cc0aca357458e5b97af
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-05-25 10:34:31 +0900

    Time: 77 ms (0%), Space: 230.8 MB (100%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.dart
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.dart
@@ -1,15 +1,9 @@
 class Solution {
   int longestPalindrome(List<String> words) {
     int res = 0;
-    bool center = false;
     final hm = <String, int>{};
     for (final w in words){
-        final rev = w.split('').reversed.join('');
-        if (!center && rev == w) {
-            res += 2;
-            center = true;
-            continue;
-        }
+        final rev = '${w[1]}${w[0]}';
         if ((hm[rev] ?? 0) > 0){
             res += 4;
             hm[rev] = hm[rev]! - 1;
@@ -17,6 +11,9 @@ class Solution {
         }
         hm[w] = (hm[w] ?? 0) + 1;
     }
-    return res;
+    final center = hm.entries.where((e) => e.value > 0)
+                             .map((e) => e.key)
+                             .any((k) => k == '${k[1]}${k[0]}');
+    return res + (center ? 2 : 0);
   }
 }
