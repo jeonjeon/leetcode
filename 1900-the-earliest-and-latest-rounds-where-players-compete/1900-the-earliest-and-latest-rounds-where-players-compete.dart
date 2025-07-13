17d8675a8bbb21f580658ca7cafc3b1654e42a93
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-07-13 16:59:55 +0900

    Time: 791 ms (100%), Space: 148.8 MB (100%) - LeetHub

--- 1900-the-earliest-and-latest-rounds-where-players-compete/1900-the-earliest-and-latest-rounds-where-players-compete.dart
+++ 1900-the-earliest-and-latest-rounds-where-players-compete/1900-the-earliest-and-latest-rounds-where-players-compete.dart
@@ -1,34 +1,54 @@
-import 'dart:math' as math;
+import 'dart:math';
 
 class Solution {
-  int minR = 28;
-  int maxR = 1;
+  int mn = 28, mx = 1;
+  late final int first;
+  late final int second;
   
-  void dfs(int mask, int round, int i, int j, int first, int second) {
-    if (i >= j) {
-      dfs(mask, round + 1, 0, 27, first, second);
-    } else if ((mask & (1 << i)) == 0) {
-      dfs(mask, round, i + 1, j, first, second);
-    } else if ((mask & (1 << j)) == 0) {
-      dfs(mask, round, i, j - 1, first, second);
-    } else if (i == first && j == second) {
-      minR = math.min(minR, round);
-      maxR = math.max(maxR, round);
-    } else {
-      if (i != first && i != second) {
-        dfs(mask ^ (1 << i), round, i + 1, j - 1, first, second);
-      }
-      if (j != first && j != second) {
-        dfs(mask ^ (1 << j), round, i + 1, j - 1, first, second);
-      }
+  void dfs(int mask, int round, int l, int r) {
+    if (mask == 0) return;
+    // print(mask);
+    // the end of a round
+    if (l <= r){
+      dfs(mask, round + 1, 27, 0);
+      return;
     }
+    // the player lost already
+    if (mask & (1 << r) == 0){
+      dfs(mask, round, l, r + 1);
+      return;
+    }
+    // the player lost already
+    if (mask & (1 << l) == 0){
+      dfs(mask, round, l - 1, r);
+      return;
+    }
+    // first vs second
+    if (r == first && l == second){
+      mn = min(mn, round);
+      mx = max(mx, round);
+      return;
+    }
+    // l is the top player(first or second)
+    if (l == first || l == second){
+      dfs(mask ^ (1 << r), round, l - 1, r + 1);
+      return;
+    }
+    // r is the top player(first or second)
+    if (r == first || r == second){
+      dfs(mask ^ (1 << l), round, l - 1, r + 1);
+      return;
+    }
+    // r player wins
+    dfs(mask ^ (1 << l), round, l - 1, r + 1);
+    // l player wins
+    dfs(mask ^ (1 << r), round, l - 1, r + 1);
   }
   
   List<int> earliestAndLatest(int n, int first, int second) {
-    minR = double.maxFinite.toInt();
-    maxR = double.minPositive.toInt();
-    
-    dfs((1 << n) - 1, 1, 0, 27, first - 1, second - 1);
-    return [minR, maxR];
+    this.first = first - 1;
+    this.second = second - 1;
+    dfs((1 << n) - 1, 1, 27, 0);
+    return [mn, mx];
   }
 }
