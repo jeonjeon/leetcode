8281d150ab085b1da1397f4af088870c84e5ee94
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-07-28 11:16:27 +0900

    Time: 54 ms (100%), Space: 149.6 MB (0%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.dart
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.dart
@@ -1,17 +1,18 @@
 class Solution {
   int countMaxOrSubsets(List<int> nums) {
     int res = 0;
-    final max = nums.reduce((a,b) => a | b);
-    final len = pow(2, nums.length).toInt();
-    for (int i = 1; i < len; i++){
+    final mx = nums.reduce((a, b) => a | b);
+    final tot = 1 << nums.length;
+    for (int i = 1; i < tot; i++){
       int cur = 0;
-      for (int j = 0; j < nums.length; j++){
-        int n = 1 << j;
-        if (i & n == n){
-          cur |= nums[j];
+      for (int j = 0; (1 << j) <= i; j++){
+        if ((1 << j) & i == 0) continue;
+        cur |= nums[j];
+        if (cur == mx) {
+          res++;
+          break;
         }
       }
-      if (cur == max) res++;
     }
     return res;
   }
