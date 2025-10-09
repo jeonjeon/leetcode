4280d3662392750c5e38221e9dc989801ac34b7d
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-10-09 13:59:57 +0900

    Time: 246 ms (100%), Space: 156.2 MB (100%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.dart
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.dart
@@ -1,17 +1,15 @@
 import 'dart:math';
 class Solution {
   int minTime(List<int> skill, List<int> mana) {
-    final m = skill.length, n = mana.length;
-    final dp = List.generate(n, (_) => List.filled(m, 0));
-    for(int i = 0; i < n; i++){
-      for (int j = 0; j < m; j++){
-        final start =  max(i == 0 ? 0 : dp[i - 1][j], j == 0 ? 0 : dp[i][j - 1]);
-        dp[i][j] = start + skill[j] * mana[i];
-      }
-      for (int j = m - 2; j >= 0; j--){
-        dp[i][j] = dp[i][j + 1] - skill[j + 1] * mana[i];
-      }
+    final n = skill.length, m = mana.length;
+    var dp = List.filled(n + 1, 0);
+    for(int i = 0; i < m; i++){
+      final mn = mana[i];
+      final newDp = List.filled(n + 1, 0);
+      for (int j = 1; j <= n; j++) newDp[j] = max(dp[j], newDp[j - 1]) + skill[j - 1] * mn;
+      for (int j = n - 1; j >= 1; j--) newDp[j] = newDp[j + 1] - skill[j] * mn;
+      dp = newDp;
     }
-    return dp.last.last;
+    return dp.last;
   }
 }
