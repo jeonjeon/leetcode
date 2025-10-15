a6b4e60256767c3e5a24a3e286ef4df84f1de183
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-10-15 09:29:33 +0900

    Time: 469 ms (100%), Space: 212.1 MB (100%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.dart
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.dart
@@ -1,27 +1,17 @@
+import 'dart:math';
 class Solution {
   int maxIncreasingSubarrays(List<int> nums) {
-    final prefix = List.filled(nums.length, 1);
+    int cur = 1, last = 0, res = 0;
     for (int i = 1; i < nums.length; i++){
         if (nums[i] > nums[i - 1]){
-            prefix[i] += prefix[i - 1];
-        }
-    }
-    int l = 1, r = nums.length ~/ 2;
-    while (l < r){
-        final k = ((l + r) / 2).ceil();
-        // print('l: $l, r: $r');
-        if (bs(prefix, k)){
-            l = k;
+            cur++;
         } else {
-            r = k - 1;
+            last = cur;
+            cur = 1;
         }
+        res = max(res, cur ~/ 2);
+        res = max(res, min(last, cur));
     }
-    return r;
-  }
-  bool bs(List<int> prefix, int k){
-    for (int i = k - 1; i < prefix.length - k; i++){
-        if (prefix[i] >= k && prefix[i + k] >= k) return true;
-    }
-    return false;
+    return res;
   }
 }
