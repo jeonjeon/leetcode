7af0e7b0e3b534503ba2028c32f65d998b25bf16
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2025-11-28 11:57:24 +0900

    Time: 104 ms (100%), Space: 223.4 MB (100%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.dart
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.dart
@@ -2,36 +2,34 @@ import 'dart:collection';
 class Solution {
   int maxKDivisibleComponents(int n, List<List<int>> edges, List<int> values, int k) {
     if (n == 1) return 1;
-    int res = 0;
+    // edges to hash map
     final hm = <int, List<int>>{};
-    final visit = <int>{};
     for (final [e1, e2] in edges){
       (hm[e1] ??= []).add(e2);
       (hm[e2] ??= []).add(e1);
     }
+    // nodes with in-degree 1
     final q = Queue<int>.from(
       hm.entries.where((e) => e.value.length == 1)
                 .map((e) => e.key)
     );
+    // valid node set
+    final valid = <int>{};
     while (q.isNotEmpty){
       final cur = q.removeFirst();
-      // if (visit.contains(cur)) continue;
-      if (!visit.contains(cur) && values[cur] % k == 0){
-        res++;
-        visit.add(cur);
-        // print('cur: $cur, res: $res');
+      if (values[cur] % k == 0){
+        valid.add(cur);
       }
+      // pass the node has no edges
       if (hm[cur]!.length != 1) continue;
       final next = hm[cur]!.first;
       values[next] += values[cur] % k;
-      // values[cur] = 0;
+      // remove edge between cur and next nodes
       hm[next]!.remove(cur);
       hm[cur]!.remove(next);
       q.add(next);
-      // print('cur: $cur, next: $next, values: $values');
     }
-    // print(hm);
-    // print(q);
-    return res;
+    // count of valid nodes
+    return valid.length;
   }
 }
