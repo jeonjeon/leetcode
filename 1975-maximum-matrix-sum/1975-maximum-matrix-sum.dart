619605e98443d216747cc21c58f75a165ae77728
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-01-05 21:33:23 +0900

    Time: 3 ms (100%), Space: 159.5 MB (100%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.dart
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.dart
@@ -1,18 +1,17 @@
 import 'dart:math';
 class Solution {
   int maxMatrixSum(List<List<int>> matrix) {
-    int mn = 100001, sum = 0;
-    bool neg = false;
-    for (final row in matrix){
-        for (final v in row){
-            if (v < 0) neg = !neg;
-            mn = min(mn, v.abs());
-            sum += v.abs();
-        }
+    int mn = 100000, neg = 0, tot = 0;
+    for (final mat in matrix){
+      for (final m in mat){
+        if (0 > m) neg++;
+        final cur = m.abs();
+        tot += cur;
+        mn = min(mn, cur);
+      }
     }
-    if (neg){
-        sum -= mn * 2;
-    }
-    return sum;
+    // print('tot: $tot, neg: $neg, mn: $mn');
+    if (neg % 2 == 0) return tot;
+    return tot - mn * 2;
   }
 }
