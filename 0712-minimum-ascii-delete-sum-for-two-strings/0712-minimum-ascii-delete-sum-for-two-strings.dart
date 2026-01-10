a7943fdd5830f66bc8cd05052e792ff39c9bbedb
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-01-10 14:20:03 +0900

    Time: 27 ms (100%), Space: 158.9 MB (100%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.dart
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.dart
@@ -1,30 +1,19 @@
 import 'dart:math';
 class Solution {
   int minimumDeleteSum(String s1, String s2) {
-    final n1 = s1.length + 1;
-    final n2 = s2.length + 1;
-    final dp = List.generate(n1, (_) => List.filled(n2, 0));
-    for (int i = 1; i < n1; i++){
-      dp[i][0] = dp[i - 1][0] + s1.codeUnitAt(i - 1);
-    }
-    for (int i = 1; i < n2; i++){
-      dp[0][i] = dp[0][i - 1] + s2.codeUnitAt(i - 1);
-    }
-    for(int i = 1; i < n1; i++){
-      for (int j = 1; j < n2; j++){
-        if (s1[i - 1] == s2[j - 1]){
-          dp[i][j] = dp[i - 1][j - 1];
-        } else {
-          dp[i][j] = min(dp[i - 1][j] + s1.codeUnitAt(i - 1), dp[i][j - 1] + s2.codeUnitAt(j - 1));
+    final m = s1.length, n = s2.length;
+    final arr1 = s1.codeUnits;
+    final arr2 = s2.codeUnits;
+    final tot = arr1.reduce((a, b) => a + b) + arr2.reduce((a, b) => a + b);
+    final dp = List.generate(m + 1, (_) => List.filled(n + 1, 0));
+    for (int i = 1; i <= m; i++){
+      for (int j = 1; j <= n; j++){
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        if (arr1[i - 1] == arr2[j - 1]){
+          dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + arr1[i - 1]);
         }
       }
     }
-    return dp.last.last;
+    return tot - dp.last.last * 2;
   }
 }
-//[
-//  [0, 101, 198, 314],
-// [115, 202, 295, 430],
-// [216, 115, 392, 546],
-// [313, 216, 115, 662]
-//]
