e8d63687b9fe1aea5c5ed945de0246077f86a8db
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-01-29 12:30:32 +0900

    Time: 64 ms (100%), Space: 159.1 MB (100%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.dart
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.dart
@@ -1,29 +1,31 @@
 import 'dart:math';
 class Solution {
-  final maxx = 2000000001;
   final a = 'a'.codeUnitAt(0);
-  int minimumCost(String s, String t, List<String> ori, List<String> ch, List<int> co) {
-    int res = 0;
-    final cost = List.generate(26, (i) => List.filled(26, maxx));
-    for (int i = 0; i < ori.length; i++){
-      cost[ord(ori[i])][ord(ch[i])] = min(cost[ord(ori[i])][ord(ch[i])], co[i]);
+  int minimumCost(String source, String target, List<String> original, List<String> changed, List<int> cost) {
+    final dp = List.generate(26, (i) => List.generate(26, (j) => i == j ? 0 : 1000000000));
+    for (int i = 0; i < cost.length; i++){
+      final org = original[i].codeUnitAt(0) - a;
+      final cng = changed[i].codeUnitAt(0) - a;
+      final cst = cost[i];
+      dp[org][cng] = min(dp[org][cng], cst);
     }
-    // floyd warshal
     for (int i = 0; i < 26; i++){
       for (int j = 0; j < 26; j++){
         for (int k = 0; k < 26; k++){
-          cost[j][k] = min(cost[j][k], cost[j][i] + cost[i][k]);
+          dp[j][k] = min(dp[j][k], dp[j][i] + dp[i][k]);
         }
       }
     }
-    for (int i = 0; i < s.length; i++){
-      if (s[i] == t[i]) continue;
-      final c = cost[ord(s[i])][ord(t[i])];
-      if (c == maxx) return -1;
-      res += c;
+    int res = 0;
+    for (int i = 0; i < source.length; i++){
+      final s = source.codeUnitAt(i) - a;
+      final t = target.codeUnitAt(i) - a;
+
+      if (dp[s][t] >= 1000000000) {
+        return -1;
+      }
+      res += dp[s][t];
     }
     return res;
   }
-
-  int ord(String w) => w.codeUnitAt(0) - a;
 }
