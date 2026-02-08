249b1f2bbd2b4145d578f389e41d6272410ccd69
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-02-08 09:53:39 +0900

    Time: 34 ms (100%), Space: 170.3 MB (100%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.dart
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.dart
@@ -1,15 +1,17 @@
 import 'dart:math';
 class Solution {
   int minimumDeletions(String s) {
-    int endWithA = 0, endWithB = 0;
+    if (s.length == 1) return 0;
+    final dp = List.filled(s.length + 1, 0);
+    int b = 0;
     for (int i = 0; i < s.length; i++){
       if (s[i] == 'a'){
-        endWithB++;
-      } else {
-        endWithB = min(endWithA, endWithB);
-        endWithA++;
+        dp[i + 1] = min(dp[i] + 1, b);
+        continue;
       }
+      dp[i + 1] = dp[i];
+      b++;
     }
-    return min(endWithA, endWithB);
+    return dp.last;
   }
 }
