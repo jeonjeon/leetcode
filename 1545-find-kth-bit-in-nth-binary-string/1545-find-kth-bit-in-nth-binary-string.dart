10bd7fe110716a10c56b18df0b1d69c405d62d51
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-03-03 16:01:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.dart
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.dart
@@ -1,11 +1,6 @@
 class Solution {
   String findKthBit(int n, int k) {
-    int res = 0, digit = 1;
-    while (digit < k) {
-      digit <<= 1;
-      digit++;
-    }
-    // print('k: $k, digit: $digit');
+    int res = 0, digit = (1 << n) - 1;
     while (digit > 1){
       final half = digit ~/ 2;
       if (k == half + 1) return '${1 ^ res}';
@@ -13,9 +8,7 @@ class Solution {
         k = digit - k + 1;
         res ^= 1;
       }
-      digit--;
       digit >>= 1;
-      // print('k: $k, digit: $digit, res: $res');
     }
     return '$res';
   }
