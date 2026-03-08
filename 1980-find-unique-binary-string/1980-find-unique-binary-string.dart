a40f4dd1251867bc938c46a0e3c8728c23424b97
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-03-08 10:18:44 +0900

    Time: 5 ms (66.67%), Space: 146.7 MB (100%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.dart
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.dart
@@ -1,9 +1,10 @@
-import 'dart:math';
 class Solution {
   String findDifferentBinaryString(List<String> nums) {
-    final bins = nums.map((n) => int.parse(n, radix: 2)).toSet();
-    for (int i = 0; i < 65536; i++){
-        if (!bins.contains(i)) return i.toRadixString(2).padLeft(nums.length, '0');
+    final len = nums[0].length;
+    final sett = nums.map((n) => int.parse(n, radix: 2)).toSet();
+    for (int n = 0; n < 1 << len; n++){
+      if (sett.contains(n)) continue;
+      return n.toRadixString(2).padLeft(len, '0');
     }
     return '';
   }
