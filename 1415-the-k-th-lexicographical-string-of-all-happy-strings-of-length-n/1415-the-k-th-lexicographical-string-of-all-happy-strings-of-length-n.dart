8bc2ce702a96322ec2552e46b969d95a863e9529
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-03-14 16:15:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.dart
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.dart
@@ -1,28 +1,35 @@
 class Solution {
-  String getHappyString(int n, int k) {
-    final a = 'a'.codeUnitAt(0);
-    final res = List.generate(n, (i) => i % 2 == 1 ? 1 : 0);
-    for (int _ = 0; _ < k - 1; _++){
-        int i = n - 1;
-        for (;i > 0; i--){
-            if (res[i] == 2) continue; // c면 한칸 앞으로
-            if (res[i] == 1 && res[i - 1] == 2) continue; // b인데 앞칸이 c면 한칸 앞으로
-            break;
+    String getHappyString(int n, int k) {
+        final res = List.generate(n, (i) => i % 2 == 1 ? 1 : 0);
+        for (int _ = 0; _ < k - 1; _++) {
+            int i = n - 1;
+            // Find the rightmost position that can be incremented
+            while (i > 0 && (res[i] == 2 || res[i] == 1 && res[i - 1] == 2)) {
+                i--;
+            }
+            
+            // If the highest possible value is reached, return an empty string
+            if (i == 0 && res[i] == 2) return "";
+
+            res[i]++; // Increment the current position
+            
+            // If the new value is the same as the previous one, increment again
+            if (i > 0 && res[i] == res[i - 1]) {
+                res[i]++;
+            }
+
+            sort(res, i + 1); // Reorder the following sequence in alternating pattern
         }
-        if (i == 0 && res[i] == 2) return '';
-        res[i]++;
-        if (i > 0 && res[i] == res[i - 1]){
-            res[i]++;
+        
+        final a = 'a'.codeUnitAt(0);
+        return res.map((r) => String.fromCharCode(r + a)).join('');
+    }
+
+    // Reset the sequence from index `si` in alternating pattern: ababab... or bababa...
+    void sort(List<int> arr, int si) {
+        for (int i = si; i < arr.length; i++) {
+            if (arr[i - 1] == 0) arr[i] = 1;
+            else arr[i] = 0;
         }
-        sort(res, i + 1);
     }
-    return res.map((r) => String.fromCharCode(r + a))
-              .join('');
-  }
-  void sort(List<int> arr, int si){
-    for (int i = si; i < arr.length; i++){
-        if (arr[i - 1] == 0) arr[i] = 1;
-        else arr[i] = 0;
-    }
-  }
 }
