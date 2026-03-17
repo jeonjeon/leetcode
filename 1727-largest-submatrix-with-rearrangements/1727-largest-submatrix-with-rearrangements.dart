3a602eebfdc55a5b71d7538cc96a223c9a1a4450
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-03-17 22:33:20 +0900

    Time: 38 ms (100%), Space: 208.8 MB (100%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.dart
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.dart
@@ -1,14 +1,13 @@
 import 'dart:math';
 class Solution {
   int largestSubmatrix(List<List<int>> matrix) {
-    for (int i = 1; i < matrix.length; i++){
-      for (int j = 0; j < matrix[0].length; j++){
+    int res = 0;
+    for(int j = 0; j < matrix[0].length; j++){
+      for (int i = 1; i < matrix.length; i++){
         if (matrix[i][j] == 0) continue;
         matrix[i][j] += matrix[i - 1][j];
       }
     }
-    // print(matrix);
-    int res = 0;
     for (int i = 0; i < matrix.length; i++){
       matrix[i].sort((a, b) => b.compareTo(a));
       for (int j = 0; j < matrix[0].length; j++){
