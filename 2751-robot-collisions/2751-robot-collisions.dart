6d263c3d97dce8e76f7ae2c5b2e35ff5433d4bce
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-04-01 10:53:52 +0900

    Time: 121 ms (100%), Space: 248.7 MB (100%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.dart
+++ 2751-robot-collisions/2751-robot-collisions.dart
@@ -1,39 +1,38 @@
 class Solution {
   List<int> survivedRobotsHealths(List<int> pos, List<int> heal, String dir) {
     final n = pos.length;
-    final arr = <(int, int, String, int)>[];
-    final res = <(int, String, int)>[];
-    int l = 0, r = 0;
+    final arr = <(int, int, int, String)>[];
     for (int i = 0; i < n; i++){
-      arr.add((pos[i], heal[i], dir[i], i));
+      arr.add((i, pos[i], heal[i],dir[i]));
     }
-    arr.sort((a, b) => a.$1.compareTo(b.$1));
-    for (final (p, h, d, i) in arr){
+    arr.sort((a, b) => a.$2.compareTo(b.$2));
+    final res = <(int, int)>[];
+    final R = <(int, int)>[];
+    for (var (i, _, h, d) in arr){
       if (d == 'R'){
-        res.add((h, d, i));
+        R.add((i, h));
         continue;
       }
-      int curH = h;
-      while (curH > 0 && res.isNotEmpty && res.last.$2 == 'R'){
-        final (prevH, prevD, prevI) = res.removeLast();
-        if (prevH < curH){
-          curH -= 1;
-          continue;
-        }
-        if (prevH > curH){
-          res.add((prevH - 1, prevD, prevI));
-          curH = 0;
+      while (R.isNotEmpty && h > 0){
+        var (ri, rh) = R.removeLast();
+        if (rh == h){
+          h = 0;
           break;
         }
-        curH = 0;
-        break;
+        if (rh > h){
+          rh--;
+          h = 0;
+          R.add((ri, rh));
+          break;
+        }
+        h--;
       }
-      if (curH > 0){
-        res.add((curH, d, i));
+      if (h > 0){
+        res.add((i, h));
       }
     }
-    res.sort((a, b) => a.$3.compareTo(b.$3));
-    //print(res);
-    return res.map((a) => a.$1).toList();
+    res.addAll(R);
+    res.sort((a, b) => a.$1.compareTo(b.$1));
+    return res.map((a) => a.$2).toList();
   }
 }
