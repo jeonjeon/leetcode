de0cafbeec8c0e61d4d91ece7f8e5e46757cb02d
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-04-15 18:41:55 +0900

    Time: 42 ms (100%), Space: 152.2 MB (100%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.dart
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.dart
@@ -1,33 +1,20 @@
-import 'dart:collection';
+import 'dart:math';
 class Solution {
-  final mx =  0x7FFFFFFFFFFFFFFF;
-  int minimumTotalDistance(List<int> robot, List<List<int>> fac) {
-    // 3D array DP
-    // 로봇과 공장이 서로 크로스 되면 안된다 최적화 안된것이기 때문
-    robot.sort();
-    final fact = fac.map((a) => List.filled(a[1], a[0]))
-                    .expand((a) => a)
-                    .toList()
-                    ..sort();
-    final r = robot.length;
-    final f = fact.length;
-    final dp = List.generate(r, (_) => List.filled(f, mx));
-    
-    for (int i = 0; i < f; i++){
-      if (f - i - 1 < r - 1) break;
-      dp[0][i] = (robot[0] - fact[i]).abs();
-    }
-
-    for (int i = 1; i < r; i++){
-      int mn = mx;
-      for (int j = i; j < f; j++){
-        if (f - j < r - i) break;
-        mn = min(mn, dp[i - 1][j - 1]);
-        final diff = (robot[i] - fact[j]).abs();
-        dp[i][j] = min(dp[i][j], mn + diff);
+  int minimumTotalDistance(List<int> rob, List<List<int>> fac) {
+    rob.sort();
+    fac.sort((a, b) => a[0].compareTo(b[0]));
+    final dp = List.generate(fac.length + 1, (i) => List.generate(rob.length + 1, (j) => j == 0 ? 0 : 200000000900));
+    for (int i = 1; i <= fac.length; i++){
+      final [pos, lim] = fac[i - 1];
+      for (int j = 1; j <= rob.length; j++){
+        dp[i][j] = dp[i - 1][j];
+        int dist = 0;
+        for (int k = 1; k <= min(lim, j); k++){
+          dist += (rob[j - k] - pos).abs();
+          dp[i][j] = min(dp[i][j], dp[i - 1][j - k] + dist);
+        }
       }
     }
-    
-    return dp.last.reduce(min);
+    return dp.last.last;
   }
 }
