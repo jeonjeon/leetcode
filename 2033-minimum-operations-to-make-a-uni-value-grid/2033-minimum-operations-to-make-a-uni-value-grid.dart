99d5c73f0c560c474cbac3fc7cfce441b17da3e2
HyoungJin Jeon <50126777+jeonjeon@users.noreply.github.com>
2026-04-28 11:12:21 +0900

    Time: 212 ms (100%), Space: 237 MB (100%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.dart
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.dart
@@ -1,8 +1,24 @@
+import 'dart:math';
 class Solution {
   int minOperations(List<List<int>> grid, int x) {
-    final arr = grid.expand((a) => a).toList()..sort();
-    if (arr.map((a) => a % x).toSet().length > 1) return -1;
-    final target = arr[arr.length ~/ 2];
-    return arr.map((a) => (a - target).abs()).reduce((a, b) => a + b) ~/ x;
+    final arr = grid.expand((n) => n).toList();
+    arr.sort();
+    final n = arr.length;
+    if (n == 1) return 0;
+    final left = List.filled(n, 0);
+    for (int i = 1; i < arr.length; i++){
+      final diff = arr[i] - arr[i - 1];
+      if (diff % x > 0) return -1;
+      left[i] = left[i - 1] + diff * i;
+    }
+    int res = arr.last * n;
+    int right = 0;
+    for (int i = n - 2; i >= 0; i--){
+      final diff = arr[i + 1] - arr[i];
+      right += diff * (n - i - 1);
+      res = min(res, left[i] + right);
+      // print('i: $i, res: $res, diff: $diff, left: ${left[i]} right: $right');
+    }
+    return res ~/ x;
   }
 }
